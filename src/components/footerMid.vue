b3bbc54b733dbe00a9fb071930718ae1aa6082ab
Aligern <sasso.luca93@gmail.com>
2024-04-10 18:58:22 +0200

    add footerMid

--- src/components/footerMid.vue
+++ src/components/footerMid.vue
@@ -1,17 +1,22 @@
 <template>
-    <div id="footerMid" class="container text-white">
-       <div> 
-        <ul  v-for="(item, index) in footerList" :key="index">
+    <div id="footerMid" >
+       <div class="container d-flex justify-content-center"> 
+        <div id="left" class="text-white d-flex">
+            <ul  v-for="(item, index) in footerList" :key="index">
             {{ item.title }}
-            <li v-for="(item, index) in item.list" :key="index">
-                {{ item.list }}
+            <li v-for="(itemlist, i) in item.list" :key="i">
+               <a href="">
+                {{ itemlist }}
+               </a>
             </li>
         </ul>
        </div>
-
-       <div>
-        right
+       <div id="right">
        </div>
+        </div>
+   
+
+    
     </div>
 </template>
 
@@ -73,10 +78,30 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
 #footerMid {
-    height: 200px;
-    display: flex;
-    justify-content: space-between;
+    background-image: url('./img/footer-bg.jpg');
+    background-size: cover;
+    ul {
+        list-style-type: none;
+        li {
+            a {
+                text-decoration: none;
+                color: rgb(136, 135, 135);
+                font-weight: 600;
+            }
+        }
+    }
+    #left {
+        padding-top: 20px;
+        width: 50%;
+    }
+    #right {
+        background-image: url('./img/dc-logo-bg.png');
+        width: 50%;
+        background-position: right center;
+        background-repeat: no-repeat;
+        background-size: 70%;
+    }
 }
-
 </style>
