86fb766cf6f6664eb5ca451240ab0e977db3cb35
Aligern <sasso.luca93@gmail.com>
2024-04-10 15:28:54 +0200

    add header

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <headerComponent />
+    <mainComponent />
+    <footerComponent />
+</template>
+
+
+
+<script>
+import headerComponent from './components/headerComponent.vue';
+import mainComponent from './components/mainComponent.vue';
+import footerComponent from './components/footerComponent.vue';
+    export default {
+        name: 'App',
+        components: {
+            headerComponent,
+            mainComponent,
+            footerComponent
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/headerComponent.vue
@@ -0,0 +1,81 @@
+<template>
+<header>
+    <div class="d-flex justify-content-between">
+        <div>
+            <img src="/img/dc-logo.png" alt="Logo">
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <a href="#">
+                        Characters
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Comics
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Movies
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        TV
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Games
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Collectibles
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Videos
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Fans
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        News
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        Shop
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
+</template>
+
+<script>
+    export default {
+        name: 'headerComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+    ul{
+    list-style-type: none;
+    display: flex;
+        li {
+            padding: 6px;
+            
+}
+}
+
+</style>
